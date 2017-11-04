61be70fb521897307010b88a3b66b7858cbbca51
MylesColeman14 <Myles.coleman14@gmail.com>
2017-11-04 10:01:34 -0500

    Update app.js
    updated app

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,9 +4,7 @@ let infowindow;
 let pyrmont;
 let myLocation;
 let localPosition;
-let contentCall;
-let priceChoice = 5;
-
+let content;
 
 $(document).ready(function(){
     initialize();
@@ -23,27 +21,22 @@ function initialize() {
   });
 
 
-
   function createMap(position, callbackMarker){
     localPosition = position;
     pyrmont = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-    map = new google.maps.Map(document.getElementById('map-display'), {
+    map = new google.maps.Map(document.getElementById('map'), {
         center: pyrmont,
         zoom: 16,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
       });
-    content = 'My Location';
-
-
-    callbackMarker(localPosition, content, map);
 
     $('.advance').on('click', function(){
-      console.log($('#start-location').val().trim());
+      console.log($('#myLocation').val().trim());
       let request = {
         location: pyrmont,
         radius: '5000',
-        keyword: [$('#start-location').val().trim()]
+        keyword: [$('#myLocation').val().trim()]
       };
 
       service = new google.maps.places.PlacesService(map);
@@ -55,8 +48,29 @@ function initialize() {
     callbackMarker(localPosition, content, map);
   }
 
-  
+  function callback(results, status) {
+    console.log(results);
+    let bounds = new google.maps.LatLngBounds();
 
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      for (let i = 0; i < results.length; i++) {
+        let place = results[i];
+        let markerLocation ={
+          coords: {
+            latitude: place.geometry.viewport.f.b,
+            longitude: place.geometry.viewport.b.b,
+          }
+        }
+        var myLatLng = new google.maps.LatLng(markerLocation.coords.latitude, markerLocation.coords.longitude);
+        bounds.extend(myLatLng);
+        map.fitBounds(bounds);
+        let content = {
+          name: results[i].name
+        }
+        content = results[i].name;
+        createMarker(markerLocation, content, map);
+      }
+    }
   }
 }
 
